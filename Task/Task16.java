89a60610028e9a087ed7f64618b1c8e56a70a572
Bryan133123 <bryanjohnlopez.133123@gmail.com>
2026-08-22 14:47:18 +0800

    Task16 completed

---
+++ Task/Task16.java
@@ -0,0 +1,12 @@
+public class Task16 {
+    public static void main(String[] args) {
+        Gorilla gorilla = new Gorilla();
+        gorilla.feed(true);
+        gorilla.groom();
+        gorilla.pet();
+        
+        System.out.println();
+        System.out.println("Task 16 Completed");
+        System.out.println("Bryan John Lopez");
+    }
+}
